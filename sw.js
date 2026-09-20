7c32e49a1105f290007d4b21b3ed104155e1a035
Mehmet Yıldız <115743794+mehmetyildiz03@users.noreply.github.com>
2026-09-21 02:03:41 +0300

    Improve offline install support

--- sw.js
+++ sw.js
@@ -1,5 +1,33 @@
-const CACHE = 'zirhova-v0.1';
-const ASSETS = ['./','./index.html','./styles.css','./src/game.js','./manifest.webmanifest'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(r => r || fetch(event.request))));
+const CACHE = 'zirhova-v0.2-install';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './src/game.js',
+  './manifest.webmanifest',
+  './icons/apple-touch-icon.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).catch(() => {
+      if (event.request.mode === 'navigate') return caches.match('./index.html');
+      return undefined;
+    }))
+  );
+});
