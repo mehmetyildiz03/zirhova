3bd731b667c6c75dcac1132154ec4d806c68a529
Mehmet Yıldız <115743794+mehmetyildiz03@users.noreply.github.com>
2026-09-23 17:13:10 +0300

    Cache v0.12 profile progression module

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zirhova-v0.11-audio-polish';
+const CACHE = 'zirhova-v0.12-profile-progression';
 const ASSETS = [
   './',
   './index.html',
@@ -13,6 +13,7 @@ const ASSETS = [
   './src/balanceSystem.js',
   './src/bossSystem.js',
   './src/audioSystem.js',
+  './src/progressionSystem.js',
   './src/editor.js',
   './editor.css',
   './editor.html',
